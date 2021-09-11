94f07a223c56da34be1346b281b323d33696673f
Krasva687 <krasva687@gmail.com>
2021-09-11 16:52:21 +0300

    io added

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,17 +1,33 @@
 package ru.sber.io
 
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
  */
 class Archivator {
 
+
+
     /**
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
-
+        val files: String = "io/logfile.log"
+        ZipOutputStream(BufferedOutputStream(FileOutputStream("logfile.zip"))).use { out ->
+                FileInputStream(files).use { fi ->
+                    BufferedInputStream(fi).use { origin ->
+                        val entry = ZipEntry(files)
+                        out.putNextEntry(entry)
+                        origin.copyTo(out, 2048)
+                    }
+                }
+            }
     }
 
     /**
@@ -19,6 +35,16 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        var zipFile = ZipFile("io/logfile.zip")
+        zipFile.use { zip ->
+            zip.entries().asSequence().forEach { entry ->
+                zip.getInputStream(entry).use { input ->
+                    File("io/unzippedLogfile.log").outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+            }
+        }
 
     }
 }
