f8bea4e04e4ce69690c986595904b93e706c03e9
Krasva687 <krasva687@gmail.com>
2021-09-12 15:24:41 +0300

    nio grep added

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,13 @@
 package ru.sber.nio
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.readLines
+import kotlin.io.path.readText
+import kotlin.io.path.useLines
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -15,6 +23,38 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
-
+        //Определяем директорию поиска
+        val file = File("io/logs")
+        val path2 = file.toPath()
+        //создаем пер для хранения совпадений и добавочной информации
+        var result = ""
+        //создаем переменную с путями к файлам и заносим их
+        var fi: ArrayList<Path> = ArrayList()
+        Files.find(path2, 4, { p, _ -> p.toString().endsWith(".log") }).forEach { fi.add(it) }
+        //начинаем цикл для прохождения по файлам
+        for (i in fi) {
+            var cnt = 0
+            var lineNum = 1
+            //считываем файл по строчкам
+            var filetext = i.readLines()
+            //проверяем условия для каждой строчки есди выполняется, пишем название файла и порядковый номер строки и строку
+            for( j in filetext.indices){
+                if(filetext[j].contains(subString) && result.length > 0 ){
+                    result = result + "\n" + i.fileName.toString() + " : " + lineNum.toString() + " : " + filetext[j]
+                }else if (filetext[j].contains(subString) && result.length == 0){
+                    result = i.fileName.toString() + " : " + lineNum.toString() + " : " + filetext[j]
+                }
+                lineNum++
+            }
+        }
+        //создаем файл в диерктории io и записываем в него информацию
+        var storePath = File("io/result.txt")
+        storePath.createNewFile()
+        storePath.writeText(result)
+        print(result)
     }
+}
+
+fun main(){
+    Grep().find("22/Jan/2001:14:27:46")
 }
